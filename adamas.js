9c3793e166a5711edc039d85603791538f40d77b
burcukaraman <burcukaramanlioglu@gmail.com>
2015-12-11 16:08:44 +0200

    ....
    ....

--- adamas.js
+++ adamas.js
@@ -3,11 +3,11 @@ var kelimeler = new Array('kütüphane','koleksiyon','veri tabanı','açık eri�
  var secilenKelimeNo = Math.floor(Math.random() * kelimeSayisi); 
  var secilenKelime = kelimeler[secilenKelimeNo]; 
  var harfSayisi = secilenKelime.length; 
- 
+ var hataSayisi = 0
  
  var harfTutucu = new Array(); 
- 
- 
+
+
  for (var j=0; j<harfSayisi; j++){ 
          harfTutucu[j] = ' _ '; 
          harfTutucu[secilenKelime.indexOf(' ',j)] = ' - '; 
@@ -15,9 +15,40 @@ var kelimeler = new Array('kütüphane','koleksiyon','veri tabanı','açık eri�
  
  
  function harfKontrol(secilenHarf){ 
-     for (var i=0; i<harfSayisi; i++){ 
-         bulunanHarfNo = secilenKelime.indexOf(secilenHarf,i); 
+//     for (var i=0; i<harfSayisi; i++){ 
+         bulunanHarfNo = secilenKelime.indexOf(secilenHarf); 
+         if (bulunanHarfNo == -1){
+          hataliysa_ara()   
+         } else {
          harfTutucu[bulunanHarfNo] = secilenHarf; 
-         document.getElementById("harfTutucuGoster").innerHTML = harfTutucu.join('&nbsp'); 
-     } 
+         document.getElementById("harfTutucuGoster").innerHTML = harfTutucu.join('&nbsp');
+         }
+    // } 
  } 
+
+function hataliysa_ara(){
+hataSayisi += 1
+console.log(hataSayisi)
+if (hataSayisi == 3){
+console.log("Vurun kellesini")
+document.getElementById('klavye').innerHTML="Klavye yerine bu yazı geldi"
+}
+adamAs()
+}
+
+function adamAs(){
+	switch(hataSayisi) {
+    	case 0:
+        	document.getElementById('adam').src="adamasmaca1.fw.png"
+        break;
+    	case 1:
+        	document.getElementById('adam').src="adamasmaca2.fw.png"
+        break;
+			case 2:
+					document.getElementById('adam').src="adamasmaca3.fw.png"
+				break;
+			case 3:
+					document.getElementById('adam').src="adamasmaca4.fw.png"
+				break;
+	}
+}
